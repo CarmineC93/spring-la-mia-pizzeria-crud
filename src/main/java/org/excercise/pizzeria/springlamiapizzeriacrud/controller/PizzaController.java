a3262ac1227ad27035eb595f49581c979b2e4516
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2023-03-30 14:40:46 +0200

    add show() to Controller, and show.html page template

--- src/main/java/org/excercise/pizzeria/springlamiapizzeriacrud/controller/PizzaController.java
+++ src/main/java/org/excercise/pizzeria/springlamiapizzeriacrud/controller/PizzaController.java
@@ -3,13 +3,17 @@ package org.excercise.pizzeria.springlamiapizzeriacrud.controller;
 import org.excercise.pizzeria.springlamiapizzeriacrud.model.Pizza;
 import org.excercise.pizzeria.springlamiapizzeriacrud.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/pizzas")
@@ -25,15 +29,17 @@ public class PizzaController {
             return "/pizzas/index";
         }
 
-/*
-        @GetMapping("/search")
-        public String search(Model model, @RequestParam(name = "q") String keyword){
-            List<Pizza>  filteredPizzas = pizzaRepository.findByNameContainingIgnoreCase(keyword);
-            model.addAttribute("list", filteredPizzas);
-            return "/pizzas/index";
+        @GetMapping("/{pizzaId}")
+        public String show(@PathVariable("pizzaId") Integer id, Model model){
+            Optional<Pizza> result = pizzaRepository.findById(id);
+            if(result.isPresent()){
+                model.addAttribute("pizza", result.get());
+                return "/pizzas/show";
+            }else{
+                //se l'id chiamato non esiste lancerà un'eccezzione
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+            }
         }
-*/
-
 
     }
 
